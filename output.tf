00598563b8ec5ab8df9491e21ff242ba193bf2f8
Raghavan R <raghavan.r@capgemini.com>
2018-08-17 13:56:03 +1000

    tag export

--- output.tf
+++ output.tf
@@ -1,3 +1,38 @@
 output "vpcid" {
     value = "${module.vpc.vpcid}"
+    default = []
+}
+
+output "vpcarn" {
+    value = "${module.vpc.vpcarn}"
+    default = []
+}
+
+output "vpcname" {
+    value = "${module.vpc.vpcname}"
+    default = []
+}
+
+output "vpc-cidr" {
+    value = "${module.vpc.vpc-cidr}"
+    default = []
+}
+
+output "main-rt" {
+    value = "${module.vpc.main-rt}"
+    default = []
+}
+
+output "default-nacl" {
+    value = "${module.vpc.default-nacl}"
+    default = []
+}
+
+output "default-sg" {
+    value = "${module.vpc.default-sg}"
+    default = []
+}
+
+output "default-rt" {
+    value = "${module.vpc.default-rt}"
 }
