4884dfb7ade46c44159913e2c0f0b71f771040e0
Raghavan R <raghavan.r@capgemini.com>
2018-08-17 12:13:09 +1000

    adding vpc file alone

--- main.tf
+++ main.tf
@@ -1,19 +1,13 @@
 //--------------------------------------------------------------------
 // Modules
-module "network" {
-  source  = "app.terraform.io/iaac-anz-private/network/aws"
+//--------------------------------------------------------------------
+// Modules
+module "vpc" {
+  source  = "app.terraform.io/iaac-anz-private/vpc/aws"
   version = "0.1.0"
 
-  aws_access_key = "${var.aws_access_key}"
-  aws_secret_key = "${var.aws_secret_key}"
-  cidr = ["10.0.0.0/16"]
+  cidr_block = ["10.0.0.0/16"]
   create_vpc = "true"
-  cross_zone_load_balancing = "true"
-  enable_dhcp_options = "true"
   env = "PoC"
-  private-app-subnet = "10.0.3.0/24"
-  private-db-subnet = "10.0.4.0/24"
-  public-frontend-subnet-primary = "10.0.1.0/24"
-  public-frontend-subnet-secondary = "10.0.2.0/24"
-  region = "${var.region}"
+  name = "Core-Network-VPC"
 }
