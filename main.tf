8a28bc5e31afa13df2f819c6372e85f46c12e6b7
Raghavan R <raghavan.r@capgemini.com>
2018-08-17 13:13:53 +1000

    exporting output from module

--- main.tf
+++ main.tf
@@ -16,7 +16,7 @@ provider "aws" {
 // Modules
 module "vpc" {
   source  = "app.terraform.io/iaac-anz-private/vpc/aws"
-  version = "0.1.1"
+  version = "0.1.2"
 
   cidr_block = ["10.0.0.0/16"]
   create_vpc = "true"
