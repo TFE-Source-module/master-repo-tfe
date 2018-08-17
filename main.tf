89c1b8c0999797747d408d63d19696949b7ac1f2
Raghavan R <raghavan.r@capgemini.com>
2018-08-17 16:55:20 +1000

    include flatten

--- main.tf
+++ main.tf
@@ -35,8 +35,8 @@ module "private-subnets" {
   subnet_count = 1
   subnet_name = "private-subnet"
   vpc_id = "${module.corevpc.vpcid}"
-  subnet_cidr_block = "10.0.2.0/24"
-  availability_zone = "${data.aws_availability_zones.available.names}"
+  subnet_cidr_block = "10.0.2.0/24", "10.0.3.0/24"
+  availability_zone = "${flatten(data.aws_availability_zones.available.*.names)}"
   create_vpc = "${var.create_vpc}"
   env = "PoC"
 }
