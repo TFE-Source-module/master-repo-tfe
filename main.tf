7014b352fa59a6adc7381b3d156ad87de4b1233e
Raghavan R <raghavan.r@capgemini.com>
2018-08-20 09:40:16 +1000

    fix

--- main.tf
+++ main.tf
@@ -33,11 +33,11 @@ module "corevpc" {
 module "private-subnets" {
   source  = "app.terraform.io/iaac-anz-private/subnet/aws"
   version = "0.1.2"
-  subnet_count = 1
+  subnet_count = 2
   subnet_name = "private-subnet"
   vpc_id = "${module.corevpc.vpcid}"
   subnet_cidr_block = ["10.0.2.0/24", "10.0.3.0/24"]
-  availability_zone = ["${data.aws_availability_zones.available.names}"]
+  availability_zone = ["${data.aws_availability_zones.available.names[0]}", "${data.aws_availability_zones.available.names[1]}", "${data.aws_availability_zones.available.names[2]}"]
   create_vpc = "${var.create_vpc}"
   env = "PoC"
 }
