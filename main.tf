b153b6d9860f0feed6e0b8b8090a9d1159416997
Raghavan R <raghavan.r@capgemini.com>
2018-08-20 09:12:02 +1000

    sample

--- main.tf
+++ main.tf
@@ -3,6 +3,7 @@ variable "aws_access_key" {}
 variable "aws_secret_key" {}
 variable "region" {}
 variable "create_vpc" {}
+
 //--------------------------------------------------------------------
 // Data sources
 data "aws_availability_zones" "available" {}
