96b97dc4b5eaf26c4053df7dadec801ec158a93e
Raghavan R <raghavan.r@capgemini.com>
2018-08-20 10:17:35 +1000

    bug fix

--- main.tf
+++ main.tf
@@ -32,7 +32,7 @@ module "corevpc" {
 
 module "private-subnets" {
   source  = "app.terraform.io/iaac-anz-private/subnet/aws"
-  version = "0.1.5"
+  version = "0.1.6"
   subnet_name = "private-subnet"
   vpc_id = "${module.corevpc.vpcid}"
   subnet_cidr_block = ["10.0.2.0/24", "10.0.3.0/24"]
