5ecb62f28cdb6010507f0fc9e5c9bcdbab0cf903
Raghavan R <raghavan.r@capgemini.com>
2018-08-22 13:47:26 +1000

    syntax fix

--- main.tf
+++ main.tf
@@ -135,7 +135,7 @@ module "private-rt-association" {
 module "igw" {
   # Configure IGW
   source                 = "app.terraform.io/iaac-anz-private/igw/aws"
-  version = "0.1.9"
+  version = "0.2.0"
   name = "Internet-gateway"
   vpc_id                 = "${module.corevpc.vpcid}"
   env                    = "PoC"
